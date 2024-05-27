8d09ec2cf81fbf3511582a4fa53e60c1df920c05
Kukaass <chestermaligaso29@gmail.com>
2024-05-28 01:54:52 +0800

    orders oks na

--- src/order/order.router.ts
+++ src/order/order.router.ts
@@ -56,3 +56,34 @@ orderRouter.post('/', [
         res.status(500).send('Error creating order');
     }
 });
+
+// UPDATE ORDER
+orderRouter.put('/:id', [
+    body('studentNumber').isString(),
+    body('studentName').isString(),
+    body('gender').isString(),
+    body('status').isString(),
+    body('orderItems').isArray(),
+    body('orderItems.*.level').isString(),
+    body('orderItems.*.productType').isString(),
+    body('orderItems.*.quantity').isNumeric(),
+    body('orderItems.*.size').isString(),
+    body('orderItems.*.unitPrice').isNumeric(),
+], async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const order = req.body;
+    try {
+        const updatedOrder = await orderServer.updateOrder(req.params.id, order, order.orderItems);
+        if (!updatedOrder) {
+            res.status(404).send('Order not found');
+        } else {
+            res.status(200).json(updatedOrder);
+        }
+    } catch (error) {
+        res.status(500).send('Error updating order');
+    }
+});
--- src/order/order.server.ts
+++ src/order/order.server.ts
@@ -22,6 +22,7 @@ type OrderItem = {
 export const getOrders = async () => {
     return await prisma.order.findMany({
         select: {
+            id: true,
             studentNumber: true,
             studentName: true,
             gender: true,
@@ -29,6 +30,16 @@ export const getOrders = async () => {
             totalPrice: true,
             createdAt: true,
             updatedAt: true,
+            orderItems: {
+                select: {
+                    level: true,
+                    productType: true,
+                    quantity: true,
+                    size: true,
+                    unitPrice: true,
+                    totalPrice: true,
+                }
+            }
         },
         
     });
@@ -41,6 +52,7 @@ export const getOrderById = async (id: string) => {
             id: id
         },
         select: {
+            id: true,
             studentNumber: true,
             studentName: true,
             gender: true,
@@ -48,6 +60,16 @@ export const getOrderById = async (id: string) => {
             totalPrice: true,
             createdAt: true,
             updatedAt: true,
+            orderItems: {
+                select: {
+                    level: true,
+                    productType: true,
+                    quantity: true,
+                    size: true,
+                    unitPrice: true,
+                    totalPrice: true,
+                }
+            }
         },
     });
 }
@@ -72,7 +94,89 @@ export const createOrder = async (order: Order, orderItems: OrderItem[]) => {
                     create: itemsWithTotalPrice,
                 },
             },
+            select: {
+                id: true,
+                studentNumber: true,
+                studentName: true,
+                gender: true,
+                status: true,
+                totalPrice: true,
+                createdAt: true,
+                updatedAt: true,
+                orderItems: {
+                    select: {
+                        level: true,
+                        productType: true,
+                        quantity: true,
+                        size: true,
+                        unitPrice: true,
+                        totalPrice: true,
+                    }
+                }
+            }
         });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// UPDATE ORDER
+export const updateOrder = async (id: string, order: Order, orderItems: OrderItem[]) => {
+    try {
+        // Ensure each orderItem has a totalPrice property
+        const itemsWithTotalPrice = orderItems.map(item => ({
+            ...item,
+            totalPrice: item.quantity * item.unitPrice
+        }));
+        
+        const totalPrice = itemsWithTotalPrice.reduce((acc, item) => acc + item.totalPrice, 0);
+        
+        const updatedOrder = await prisma.order.update({
+            where: {
+                id: id
+            },
+            data: {
+                studentName: order.studentName,
+                studentNumber: order.studentNumber,
+                gender: order.gender,
+                status: order.status,
+                totalPrice: totalPrice,   
+            },
+            include: {
+                orderItems: true
+            }
+        });
+    
+        if (updatedOrder.status === 'CLAIMED') {
+            for (const item of order.orderItems) {
+                const inventoryItem = await prisma.inventory.findFirst({
+                    where: {
+                        productType: item.productType,
+                        size: item.size,
+                        level: item.level
+                    }
+                });
+    
+                if (!inventoryItem) {
+                    console.log(`No inventory item found for productType: ${item.productType}, size: ${item.size}, level: ${item.level}`);
+                } else {
+                    // Update the quantity of the inventory item
+                    await prisma.inventory.update({
+                        where: { id: inventoryItem.id },
+                        data: { quantity: inventoryItem.quantity - item.quantity }
+                    });
+                }
+            }
+
+            // Delete all orderItems associated with the order
+            await prisma.orderItem.deleteMany({
+                where: {
+                    orderId: id
+                }
+            });
+        }
+    
+        return order;
     } catch (error) {
         console.error(error);
     }
